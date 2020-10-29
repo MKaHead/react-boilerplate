0d2dfd246106df0ae63754f1306fafa47282f131
Marco Kruse <marco.kruse@devahead.de>
2020-10-29 11:53:43 +0100

    Add new boilerplate v4

--- src/components/App/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from 'components/App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const divElement = getByText(/learn react/i);
-  expect(divElement).toBeInTheDocument();
-});
